dd1de1666ee054ae52e167e9883915c91fc947dc
wansiklee <dhkstlr2662@gmail.com>
2020-02-11 21:57:02 +0900

    Express Middlewares

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app = express();
 
 const PORT = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("홈");
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}!`);

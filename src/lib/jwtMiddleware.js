74f76294184fdf8140caaa298971a11e6ea949b2
wansiklee <dhkstlr2662@gmail.com>
2020-02-14 13:28:09 +0900

    Reissuance JWT

--- src/lib/jwtMiddleware.js
+++ src/lib/jwtMiddleware.js
@@ -10,6 +10,17 @@ const jwtMiddleware = async (req, res, next) => {
       _id: decoded._id,
       username: decoded.username
     };
+
+    // Reissuance jwt if token's exp less than 3days
+    const now = Math.floor(Date.now() / 1000);
+    if (decoded.exp - now < 60 * 60 * 24 * 3) {
+      const user = await User.findById(decoded._id);
+      const token = user.generateToken();
+      res.cookie("jwt", token, {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+      });
+    }
     return next();
   } catch (e) {
     console.log(e);

c83d0896b3f722eea8d45eeec66f5b7671acb738
wansiklee <dhkstlr2662@gmail.com>
2020-02-14 17:39:49 +0900

    Simple Product Model

---
+++ src/db/models/Product.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const Product = new Schema({
+  title: String,
+  description: String,
+  price: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+  /*
+    To Do
+    -file URL
+    -User
+    -Category
+    -Views
+    -Comments
+    -Likes
+  */
+});
+
+export default mongoose.model("Product", Product);

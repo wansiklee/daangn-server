e958f4baf65fd1b6c684facf06e656381195fc75
wansiklee <dhkstlr2662@gmail.com>
2020-03-11 19:53:08 +0900

    Create Multer diskStorage

--- src/lib/uploadMiddleware.js
+++ src/lib/uploadMiddleware.js
@@ -1,10 +1,18 @@
 import multer from "multer";
 
-const multerImage = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  }
+});
 
-export const uploadImage = multerImage.array("image", 5);
+export const uploadImage = multer({ storage }).single("image");
 
 export const uploadController = (req, res) => {
-  const { files } = req;
-  return res.json({ files });
+  const { file } = req;
+  console.log(file);
+  return res.json({ file });
 };
